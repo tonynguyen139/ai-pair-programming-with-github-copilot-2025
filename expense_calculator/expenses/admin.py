d00752c736cc206f97604cea3ae54f35013e2ff1
Tony <kemnhat139@gmail.com>
2025-08-30 09:40:08 +0700

    Added the expenses model and code

--- expense_calculator/expenses/admin.py
+++ expense_calculator/expenses/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Expense
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):       
+    """
+    These are Expense model fields
+    name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    """  
+    list_display = ('name', 'amount', 'timestamp', 'category')
+    search_fields = ('name', 'category__name')
+    list_filter = ('timestamp',)
--- expense_calculator/expenses/models.py
+++ expense_calculator/expenses/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Expense(models.Model):        
+    name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    #category choice fields
+    CATEGORY_CHOICES = (
+        ('food', 'Food'),
+        ('transportation', 'Transportation'),
+        ('utilities', 'Utilities'),
+        ('entertainment', 'Entertainment'),
+        ('other', 'Other'),
+    )   
+    #category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
+    
+    def __str__(self):
+        return f"{self.description} - {self.amount}"
